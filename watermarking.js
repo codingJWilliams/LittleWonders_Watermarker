8c84e62f268716b235768fefc19b16c6396ad34e
Jay Williams <course@jaywilliams.me>
2017-10-22 18:29:00 +0100

    Kool it works

--- watermarking.js
+++ watermarking.js
@@ -1,8 +1,48 @@
 var fs = require('fs')
-, gm = require('gm');
+  , gm = require('gm');
+var resolve = require('path').resolve;
+const ProgressPromise = require('progress-promise');
 
-var readStream = fs.createReadStream('./test.jpg');
-gm(readStream, 'test.jpg')
-.write('reformat.png', function (err) {
-  if (!err) console.log('done');
-});
+function watermark(file, output) {
+  return new Promise( function (resolve, reject) {
+    gm(file)
+    .size( (err, v) => {
+      var isize = v;
+      console.log(isize)
+      gm(file)
+      .fontSize( Math.round(isize.height / 12) )
+      .font("Consolas")
+      .fill("#7caeff")
+      .drawText(0, isize.height / 2, "PROOF ".repeat(30))
+      .resize(isize.width / 4, isize.height / 4)
+      .write(output, (err) => {
+        if (!err) {
+          resolve()
+        } else {
+          reject(err)
+        }
+      })
+    });
+  })  
+}
+function batchWatermark(filesInFolder, filesOutFolder) {
+  var filesInFolder = resolve(filesInFolder);
+  var filesOutFolder = resolve(filesOutFolder);
+  return new Promise((resolve, reject) => {
+    fs.readdir(filesInFolder, (err, files) => {
+      // Files is now a list of all baby photos
+      var thePromised = [];
+      for ( var i = 0; i < files.length; i++ ) {
+        var file = files[i];
+        var inPath = require("path").join(filesInFolder, file);
+        var outPath = require("path").join(filesInFolder, "..", "out", i.toString() + ".jpg");
+        console.log(inPath, outPath);
+        thePromised.push(watermark(inPath, outPath));
+      }
+      Promise.all(thePromised).then( (res) => {
+        console.log("Dun")
+      })
+    })
+  })
+}
+batchWatermark("./in", "./in")
