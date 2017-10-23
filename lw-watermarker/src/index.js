1353f00c9a0ef7d54b5d10d4e7ec8a07d32269a5
Jay Williams <course@jaywilliams.me>
2017-10-23 12:26:35 +0100

    j

--- lw-watermarker/src/index.js
+++ lw-watermarker/src/index.js
@@ -1,57 +1,80 @@
-import { app, BrowserWindow } from 'electron';
+const {app, BrowserWindow, ipcMain, protocol} = require('electron');
+const path = require('path')
+const url = require('url')
+var wm = require("./watermarking");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let win
 
-const createWindow = () => {
+function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+  win = new BrowserWindow({
+    width: 400, 
+    height: 800,
+    resizable: false,
+    alwaysOnTop: true,
+    x: 1520,
+    y: 0,
+    frame: true,
+    minimizable: false,
+    maximizable: false,
+    thickFrame: true
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  win.setMenu(null);
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
+  //win.webContents.openDevTools()
+  protocol.registerFileProtocol('local', (request, callback) => {
+    const url = request.url.substr(8)
+    callback({path: path.normalize(`${__dirname}/${url}`)})
+  }, (error) => {
+    if (error) console.error('Failed to register protocol')
+  })
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-};
+    win = null
+  })
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
+  // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
+  // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
+  if (win === null) {
+    createWindow()
   }
+})
+ipcMain.on('async', (event, arg) => {  
+  wm.batch(arg).then( () => {
+    event.sender.send('async-reply', {status: "done"});
+  })
+  // Reply on async message from renderer process
+  
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
